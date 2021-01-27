fbf80466bb73e100d7296542a6e3f9f692566f4d
mrloulass <louislassegue@gmail.com>
2021-01-26 22:22:49 -0800

    Code setup with files

---
+++ js/app.js
@@ -0,0 +1,40 @@
+'use strict';
+
+console.log('Hello World');
+
+let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+
+let seattle = {
+  name: 'Seattle',
+  min: 23,
+  max: 65,
+  avg: 6.3,
+};
+
+let tokyo = {
+  name: 'Tokyo',
+  min: 23,
+  max: 65,
+  avg: 6.3,
+};
+
+let dubai = {
+  name: 'Dubai',
+  min: 23,
+  max: 65,
+  avg: 6.3,
+};
+
+let paris = {
+  name: 'Paris',
+  min: 23,
+  max: 65,
+  avg: 6.3,
+};
+
+let lima = {
+  name: 'Lima',
+  min: 23,
+  max: 65,
+  avg: 6.3,
+};
